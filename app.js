1cbfcb26dade0c98ed4858cfb14925ca315d7e98
Donato Rimenti <donatohan.rimenti@gmail.com>
2021-10-22 21:16:48 +0200

    Improved styles

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ $("document").ready(() => {
       : element.testo.replace(/./g, "?");
 
     $("#content").append(
-      `<div class='row justify-content-center'><h4>${escapeHtml(
-        element.titolo
-      )}</h4></div><div class="row justify-content-center"><p>${text}</p></div>`
+      `<div class="row justify-content-center">
+        <div class="col-2 text-right">
+          <img src="assets/logo.png" alt="logo" width="40"/>
+        </div>
+        <div class="col-6">
+          <div class="row justify-content-center">
+            <h4 class="col text-left">${escapeHtml(element.titolo)}</h4>
+          </div>
+          <div class="row justify-content-center">
+            <p class="col text-left">${text}</p>
+          </div>
+           <hr class="hr-white"/>
+        </div>
+       `
     );
   });
 
