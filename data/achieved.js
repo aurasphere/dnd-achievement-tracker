34490bcd7776959d53605ad7939b0b2f8ae9ec2b
Evenfall90 <93135212+Evenfall90@users.noreply.github.com>
2021-10-25 16:53:24 +0200

    Update achieved.js
    corretto nome PG Eli

--- data/achieved.js
+++ data/achieved.js
@@ -2,7 +2,7 @@ const achieved = {
   "Dona <C>": [3, 4, 9, 19, 27, 74, 154],
   "Lele <Voth>": [3, 4, 8, 19, 27],
   "Manu <Rollo>": [8],
-  "Eli <Daghrama>": [3, 9, 19, 47],
+  "Eli <Dahgrama>": [3, 9, 19, 47],
   "Marco <Kalor>": [3, 9, 19, 47, 51, 154],
   "Anto <Romiao>": [3, 4, 8, 19, 47],
 };
