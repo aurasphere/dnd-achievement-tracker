d3b0cfd69f430848c08a252e78babef84dde01ee
Evenfall90 <93135212+Evenfall90@users.noreply.github.com>
2021-10-25 19:41:52 +0200

    Update achievements.js

--- data/achievements.js
+++ data/achievements.js
@@ -927,6 +927,6 @@ const achievements = [
  "progressivo": "154",
  "titolo": "<Ops... Not My Fault!>",
  "testo": "<Activate a trap after a failed check>",
-"visible":  false
-}
+ "visible":  false
+},
 ];
