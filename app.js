464556e9c3c1a83eeab1fc44272f8ffd39a3d3d1
Donato Rimenti <donatohan.rimenti@gmail.com>
2021-10-25 12:17:26 +0200

    Added character names

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 $("document").ready(() => {
-  const achievedUnifiedArray = Object.values(achieved).reduce(
-    (o, n) => o.concat(n),
-    []
-  );
-
   achievements.forEach((element) => {
-    const achieved = achievedUnifiedArray.includes(
-      parseInt(element.progressivo)
-    );
+    const obtainedBy = Object.entries(achieved)
+      .filter(([name, achievements]) =>
+        achievements.includes(parseInt(element.id))
+      )
+      .map(([name, achievements]) => name);
 
     const text =
-      element.visible || achieved
-        ? escapeHtml(element.testo)
-        : element.testo.replace(/./g, "?");
+      element.visible || obtainedBy.length
+        ? escapeHtml(element.text)
+        : element.text.replace(/./g, "?");
 
-    const logoHtml = achieved
+    const logoHtml = obtainedBy.length
       ? '<img src="assets/logo.png" alt="logo" width="40"/>'
       : "";
+
+    const obtainedByHtml = obtainedBy
+      .map(
+        (n) =>
+          `<div class="row justify-content-center text-center"><p class="col m-0">${escapeHtml(
+            n
+          )}</p></div>`
+      )
+      .join("");
 
     $("#content").append(
       `<div class="row justify-content-center align-items-center">
@@ -25,12 +31,15 @@ $("document").ready(() => {
         </div>
         <div class="col-4">
           <div class="row justify-content-center">
-            <h4 class="col text-left">${escapeHtml(element.titolo)}</h4>
+            <h4 class="col text-left">${escapeHtml(element.title)}</h4>
           </div>
           <div class="row justify-content-center">
             <p class="col text-left">${text}</p>
           </div>
         </div>
+         <div class="col-2">
+         ${obtainedByHtml}
+         </div>
          <div class="row justify-content-center">
             <img class="col-6" src="assets/separator.png" alt="separator"/>
           </div>
